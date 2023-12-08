89cb610cbe1e4ab9f0cfae6aa7ff537eb53f6cea
Sidachenko Michael <michaellux@yandex.ua>
2023-12-08 14:34:13 +0300

    Add project files.

---
+++ ClientApp/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app',
+    template: `<label>Choose name:</label>
+                 <input [(ngModel)]="name" placeholder="name">
+                 <h2>Welcome {{name}}!</h2>`
+})
+export class AppComponent {
+    name = '';
+}
